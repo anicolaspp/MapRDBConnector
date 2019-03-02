690a8d0f46b1dfde07f0da50355211b604160a08
Nicolas A Perez <anicolaspp@gmail.com>
2019-03-01 23:00:14 -0500

    Refactoring

--- src/main/scala/com/github/anicolaspp/spark/sql/MapRDB.scala
+++ src/main/scala/com/github/anicolaspp/spark/sql/MapRDB.scala
@@ -9,16 +9,11 @@ object MapRDB {
   implicit class ExtendedSession(sparkSession: SparkSession) {
 
     def loadFromMapRDB(path: String, schema: StructType): DataFrame = {
-
-
-      // maybe find table partition information here and send it down to the data source
-
       sparkSession
         .read
         .format("com.github.anicolaspp.spark.sql.Reader")
         .schema(schema)
         .load(path)
-
     }
   }
 

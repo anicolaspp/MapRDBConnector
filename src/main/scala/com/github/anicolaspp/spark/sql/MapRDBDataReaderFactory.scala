6a06855f9c68964c10f5eb61eb1e0a8c94bdefea
iulianov <iulianov@gmail.com>
2019-03-01 17:54:03 -0800

    Checkpoint. All types done except List, Map, Interval.

--- src/main/scala/com/github/anicolaspp/spark/sql/MapRDBDataReaderFactory.scala
+++ src/main/scala/com/github/anicolaspp/spark/sql/MapRDBDataReaderFactory.scala
@@ -4,7 +4,8 @@ import org.apache.spark.internal.Logging
 import org.apache.spark.sql.Row
 import org.apache.spark.sql.sources._
 import org.apache.spark.sql.sources.v2.reader.{DataReader, DataReaderFactory}
-import org.apache.spark.sql.types.{DataType, StructType}
+import org.apache.spark.sql.types._
+import org.ojai.{Document,Value}
 
 class MapRDBDataReaderFactory(table: String,
                               filters: List[Filter],
@@ -60,8 +61,8 @@ class MapRDBDataReaderFactory(table: String,
 
       log.trace(document.asJsonString())
 
-      val values = projectionsNames
-        .foldLeft(List.empty[String])((xs, name) => document.getString(name) :: xs)
+      val values = schema.fields
+        .foldLeft(List.empty[Any])((xs, field) => getAnyFromDocument(document, field.name, field.dataType) :: xs)
         .reverse
 
       Row.fromSeq(values)
@@ -70,6 +71,36 @@ class MapRDBDataReaderFactory(table: String,
     override def close(): Unit = {
       store.close()
       connection.close()
+    }
+  }
+
+  private def getAnyFromDocument(doc: Document, name: String, dataType: DataType ) = {
+    dataType match {
+      case BinaryType => doc.getBinary(name)
+      case BooleanType => doc.getBooleanObj(name)
+      case ByteType => doc.getByteObj(name)
+      case DateType => doc.getDate(name)
+      case _:DecimalType => doc.getDecimal(name)
+      case DoubleType => doc.getDoubleObj(name)
+      case FloatType => doc.getFloatObj(name)
+      case IntegerType => doc.getIntObj(name)
+      case ArrayType(_,_) => doc.getList(name) //TODO Need to check what happens in the regular connector
+      case LongType => doc.getLongObj(name)
+      case StructType(_) => doc.getMap(name) //TODO This might need to be done recursively based on the values in the StructType
+      case ShortType => doc.getShortObj(name)
+      case StringType => doc.getString(name)
+      case TimestampType => {
+        val valObj = doc.getValue(name)
+        if(valObj.getType == Value.TYPE_CODE_TIME) {
+          new java.sql.Timestamp(valObj.getTime.getMilliSecond)
+        } else {
+          new java.sql.Timestamp(valObj.getTimestamp.getMilliSecond)
+        }
+      }
+        //TODO this will probably not automatically convert from OInterval to CalendarIntervalType
+        // It seems that this will need to be convert to a correct string representation
+      case CalendarIntervalType => doc.getInterval(name)
+      case _ => throw new IllegalArgumentException(s"Unknown type ${dataType} for field ${name}")
     }
   }
 

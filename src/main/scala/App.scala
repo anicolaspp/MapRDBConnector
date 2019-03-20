e298a938c4c8e967ad4e403977bc09f0a02f8e75
Nicolas A Perez <anicolaspp@gmail.com>
2019-03-20 14:05:33 -0700

    This load correctly, but runs out of heap space on the driver

--- src/main/scala/App.scala
+++ src/main/scala/App.scala
@@ -1,6 +1,7 @@
 package com.github.anicolaspp
 
-import org.apache.spark.sql.SparkSession
+import org.apache.spark.rdd.RDD
+import org.apache.spark.sql.{Row, SparkSession}
 import org.apache.spark.sql.types.{IntegerType, StringType, StructField, StructType}
 
 
@@ -21,43 +22,31 @@ object App {
     sparkSession.conf.set("spark.sql.streaming.checkpointLocation", "/Users/nperez/check")
     sparkSession.conf.set("spark.sql.streaming.schemaInference", value = true)
 
-//    sparkSession.sparkContext.setLogLevel("DEBUG")
+    //    sparkSession.sparkContext.setLogLevel("DEBUG")
 
     println("HERE")
 
-    val schema = StructType(Seq(StructField("_id", StringType), StructField("_2", StringType)))//StructField("first_name", StringType), StructField("uid", StringType)))
-
-    val data = sparkSession
-      .loadFromMapRDB("/user/mapr/tables/from_parquet", schema)
-      .filter("_2 = 'n2078258460719121947'")
-//      .filter("uid = '101'")
-//      .select("_id", "first_name")
-
-
-    println(s"MY SCHEMA: ${data.schema}")
-
-    data.show()
-    
-//    sparkSession
-//      .loadFromMapRDB("/user/mapr/tables/data", schema)
-//      .filter("uid = '101' and first_name = 'tom'")
-//      .show()
+    val schema = StructType(Seq(StructField("_id", StringType), StructField("payload", StringType))) //StructField("first_name", StringType), StructField("uid", StringType)))
 //
-//    sparkSession
-//      .loadFromMapRDB("/user/mapr/tables/data", schema)
-//      .filter("uid = '101'")
-//      .filter("first_name = 'tom'")
-//      .show()
+//        val data = sparkSession
+//          .loadFromMapRDB("/user/mapr/tables/from_parquet", schema)
+//          .filter("payload = '67-34859-102-69068124-28-931853-80564775-5573-4-552141-1162559-508125-8114-59-498052941-123-7085126119-8020-460105-36-56-126-3067-338569-14116-120102731011680117-47-8778-48-1121074158-47-111-4868424292971077-105120-1711-404-6-5696-11445052-6664-54-11739'")
+//          .select("_id", "payload")
 //
-//    sparkSession
-//      .loadFromMapRDB("/user/mapr/tables/data", schema)
-//      .filter("(uid <= '101' or first_name = 'john') and _id = '1'")
-//      .show()
+//        println(s"MY SCHEMA: ${data.schema}")
 //
-//    sparkSession
-//      .loadFromMapRDB("/user/mapr/tables/data", schema)
-//      .filter("uid >= '101' or _id = '1'")
-//      .show()
+//        println(s"COUNT: ${data.count()}")
 
+
+    val rdd = sparkSession.sparkContext.parallelize(1 to 1000000).map(n => Row(n.toString))
+      .union(sparkSession.sparkContext.parallelize(List(Row("67-34859-102-69068124-28-931853-80564775-5573-4-552141-1162559-508125-8114-59-498052941-123-7085126119-8020-460105-36-56-126-3067-338569-14116-120102731011680117-47-8778-48-1121074158-47-111-4868424292971077-105120-1711-404-6-5696-11445052-6664-54-11739"))))
+      .repartition(200)
+
+    val df = sparkSession.createDataFrame(rdd, new StructType().add("value", StringType))
+
+    val joint = df.joinWithMapRDBTable("/user/mapr/tables/from_parquet", schema, "value", "payload")(sparkSession)
+
+    joint.printSchema()
+    joint.show(10)
   }
 }
--- src/main/scala/com/github/anicolaspp/spark/sql/MapRDB.scala
+++ src/main/scala/com/github/anicolaspp/spark/sql/MapRDB.scala
@@ -2,6 +2,7 @@ package com.github.anicolaspp.spark.sql
 
 import org.apache.spark.sql.types.StructType
 import org.apache.spark.sql.{DataFrame, SparkSession}
+import org.ojai.store.DriverManager
 
 
 object MapRDB {
@@ -15,15 +16,84 @@ object MapRDB {
         .schema(schema)
         .load(path)
     }
-
-//    def loadFromMapRDB(path: String, schema: StructType, idxs: String*): DataFrame = {
-//      sparkSession
-//        .read
-//        .format("com.github.anicolaspp.spark.sql.Reader")
-//        .option("idx", idxs.mkString(","))
-//        .schema(schema)
-//        .load(path)
-//    }
   }
 
+
+  implicit class DataFrameOps(dataFrame: DataFrame) {
+    def joinWithMapRDBTable(maprdbTable: String, schema: StructType, left: String, right: String)(session: SparkSession): DataFrame = {
+      import org.apache.spark.sql.functions._
+      import collection.JavaConversions._
+
+      val queryToRight = dataFrame
+        .cache()
+        .select(left)
+        .withColumnRenamed(left, "a")
+        .distinct()
+
+      val queries = queryToRight.rdd.mapPartitions { partition =>
+
+        if (partition.isEmpty) List.empty.iterator else {
+          val connection = DriverManager.getConnection("ojai:mapr:")
+
+          val values = partition.map { row =>
+            val (idx, dtype) = (schema.fieldIndex(right), schema.fields(schema.fieldIndex(right)).dataType)
+
+            val x = com.mapr.db.spark.sql.utils.MapRSqlUtils.convertToDataType(row.get(0), dtype)
+
+            x
+          }
+
+          val inCondition = connection
+            .newCondition()
+            .in(right, values.toList)
+            .build()
+            .asJsonString()
+
+          List(inCondition).iterator
+        }
+      }
+        .collect()
+        .filterNot(_ == "false") // this is a query per partition => no that many so we should collect without issues
+
+      println(s"THERE ARE: ${queries.length} QUERIES TO BE EXECUTED")
+
+      val rightDF = session
+        .read
+        .format("com.github.anicolaspp.spark.sql.ReaderWithQueries")
+        .option("queries", queries.mkString(","))
+        .schema(schema)
+        .load(maprdbTable)
+
+      dataFrame.join(rightDF, col(left) === col(right))
+    }
+  }
+
+}
+
+
+object OJAILoader {
+  def loadRowsWithIds(ids: DataFrame, tablePath: String, schema: StructType)(implicit session: SparkSession): DataFrame = {
+
+    import session.implicits._
+
+    val jsonRows = ids.mapPartitions { partition =>
+      val ojaiConnection = DriverManager.getConnection("ojai:mapr:")
+      val store = ojaiConnection.getStore(tablePath)
+
+      val outList = partition.foldLeft(List.empty[String]) { (result, nextId) =>
+
+        val documentWithId = store.findById(nextId.get(0).toString)
+
+        documentWithId.asJsonString :: result
+      }
+
+      store.close()
+      ojaiConnection.close()
+
+      outList.iterator
+    }.rdd
+
+
+    session.read.schema(schema).json(jsonRows.toDS())
+  }
 }

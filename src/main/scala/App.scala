7023e868972b07385817f9dab24dc6cea6a33477
Nicolas A Perez <anicolaspp@gmail.com>
2019-03-20 20:02:33 -0700

    merge

--- src/main/scala/App.scala
+++ src/main/scala/App.scala
@@ -22,25 +22,14 @@ object App {
     sparkSession.conf.set("spark.sql.streaming.checkpointLocation", "/Users/nperez/check")
     sparkSession.conf.set("spark.sql.streaming.schemaInference", value = true)
 
-        sparkSession.sparkContext.setLogLevel("WARN")
+    sparkSession.sparkContext.setLogLevel("WARN")
 
     println("HERE")
-
-    val schema = StructType(Seq(StructField("_id", StringType), StructField("payload", StringType))) //StructField("first_name", StringType), StructField("uid", StringType)))
-//
-//        val data = sparkSession
-//          .loadFromMapRDB("/user/mapr/tables/from_parquet", schema)
-//          .filter("payload = '67-34859-102-69068124-28-931853-80564775-5573-4-552141-1162559-508125-8114-59-498052941-123-7085126119-8020-460105-36-56-126-3067-338569-14116-120102731011680117-47-8778-48-1121074158-47-111-4868424292971077-105120-1711-404-6-5696-11445052-6664-54-11739'")
-//          .select("_id", "payload")
-//
-//        println(s"MY SCHEMA: ${data.schema}")
-//
-//        println(s"COUNT: ${data.count()}")
 
 
     val rdd = sparkSession.sparkContext.parallelize(1 to 1000000).map(n => Row(n.toString))
       .union(sparkSession.sparkContext.parallelize(List(Row("67-34859-102-69068124-28-931853-80564775-5573-4-552141-1162559-508125-8114-59-498052941-123-7085126119-8020-460105-36-56-126-3067-338569-14116-120102731011680117-47-8778-48-1121074158-47-111-4868424292971077-105120-1711-404-6-5696-11445052-6664-54-11739"))))
-    
+
 
     val df = sparkSession.createDataFrame(rdd, new StructType().add("value", StringType))
 

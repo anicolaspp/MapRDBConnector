92a45eda4c5b901a5705d66aa18f3bd6edee5f48
Nicolas A Perez <anicolaspp@gmail.com>
2020-02-11 16:25:01 -0500

    Support for Java types filtering

--- src/main/scala/com/github/anicolaspp/spark/sql/reading/SupportedFilterTypes.scala
+++ src/main/scala/com/github/anicolaspp/spark/sql/reading/SupportedFilterTypes.scala
@@ -6,14 +6,21 @@ object SupportedFilterTypes {
 
   private lazy val supportedTypes = List[Class[_]](
     classOf[Double],
+    classOf[java.lang.Double],
     classOf[Float],
+    classOf[java.lang.Float],
     classOf[Int],
+    classOf[Integer],
     classOf[Long],
+    classOf[java.lang.Long],
     classOf[Short],
+    classOf[java.lang.Short],
     classOf[String],
     classOf[Timestamp],
     classOf[Boolean],
-    classOf[Byte]
+    classOf[java.lang.Boolean],
+    classOf[Byte],
+    classOf[java.lang.Byte]
   )
 
   def isSupportedType(value: Any): Boolean = supportedTypes.contains(value.getClass)
--- version.sbt
+++ version.sbt
@@ -1,1 +1,1 @@
-version in ThisBuild := "1.0.12-SNAPSHOT"
+version in ThisBuild := "1.0.12-spark-2.3.2"
